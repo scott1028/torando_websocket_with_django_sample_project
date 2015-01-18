e774cc71c7d1e23e49ab296ed5e184186344db06
scott1028 <mic1028002@gmail.com>
2015-01-18 15:52:08 +0800

    add auto re connection 機制

--- server.py
+++ server.py
@@ -70,7 +70,7 @@ class EchoWebSocket(websocket.WebSocketHandler):
         self.write_message(data)
         self.write_message('test second message sent')
         self.write_message(str(WS_CON_POOL))
-        self.write_message('\x00\x00\x00\x65', binary=True)
+        # self.write_message('\x20', binary=True)
 
 
         # 
